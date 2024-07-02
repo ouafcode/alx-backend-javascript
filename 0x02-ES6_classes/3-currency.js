009c1e6bfd6117956776230a15ecbe25b0e27f07
ouafcode <ouafaesaim@gmail.com>
2024-07-02 15:22:46 +0000

    task3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,11 +1,5 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    } else if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-
     this._code = code;
     this._name = name;
   }
@@ -14,22 +8,16 @@ export default class Currency {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = value;
+  set name(name) {
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(value) {
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = value;
+  set code(code) {
+    this._code = code;
   }
 
   displayFullCurrency() {
