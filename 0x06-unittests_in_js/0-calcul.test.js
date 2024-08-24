17b16e6af85fa722d2733c24c0dd7e7c3cd1997b
ouafcode <ouafaesaim@gmail.com>
2024-08-24 17:16:15 +0000

    task0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const calculateNumber = require("./0-calcul.js");
 
 describe("calculation Number",() => {
   it("checks equality", () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(-1, -3), -4);
   });
 
   it("checks equality with float nbr", () => {
---
+++ 0x06-unittests_in_js/main0.js
@@ -0,0 +1,2 @@
+const calculateNumber = require('./0-calcul.js');
+console.log(calculateNumber(1.2, 3.7));
