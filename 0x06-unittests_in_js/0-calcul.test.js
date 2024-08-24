544b39b935f3751d16f66f5d84c495e7d92c6c75
ouafcode <ouafaesaim@gmail.com>
2024-08-24 17:29:08 +0000

    task0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,11 +2,15 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
 describe("calculation Number",() => {
+  it("checks equality first number rounded", () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
+  });
+
   it("checks equality", () => {
     assert.strictEqual(calculateNumber(-1, -2.7), -4);
   });
 
   it("checks equality with float nbr", () => {
-    assert.strictEqual(calculateNumber(1.7, 3), 5);
+    assert.strictEqual(calculateNumber(1.7, 3.8), 6);
   });
 });
