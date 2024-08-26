171ba261c2931c2aeb3e855a117cec9252aa13cc
ouafcode <ouafaesaim@gmail.com>
2024-08-26 21:44:56 +0000

    task5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,24 @@
+const sinon =  require('sinon');
+const sendPaymentRequestToApi = require('./5-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+  let spy;
+
+  beforeEach(function() {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    spy.restore();
+  )};
+
+  it('check for the console logging is 120', () => {
+    sendPaymentRequestToAPI(100, 20);
+    sinon.assert.calledOnceWithExactly(spy, 'The total is: 120');
+  )};
+
+  it('check for the console logging is 20', () => {
+    sendPaymentRequestToAPI(10, 10);
+    sinon.assert.calledOnceWithExactly(spy, 'The total is: 20');
+  )}; 
+)};
