31c9922d1eeb87a26bf314519f09d66aa40aa88a
ouafcode <ouafaesaim@gmail.com>
2024-08-26 16:27:46 +0000

    task3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const SUM = Utils.calculateNumber('SUM', totalAmount, totalShipping)
+  const SUM = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${SUM}`);
-  }
+}
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,7 +4,7 @@ const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', () => {
   it('check for the use of calculatNumber' , () => {
-    const spy = sinon.spy(Utils, calculateNumber);
+    const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     sinon.assert.callwith(spy, 'SUM', 100, 20);
     spy.restore();
