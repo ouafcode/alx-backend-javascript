34df7111dbfd68c1636dd6d67adda6886ad4596d
ouafcode <ouafaesaim@gmail.com>
2024-08-26 16:33:50 +0000

    task3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,10 +3,10 @@ const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', () => {
-  it('check for the use of calculatNumber' , () => {
+  it('check for the use of calculatNumber', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    sinon.assert.callwith(spy, 'SUM', 100, 20);
+    sinon.assert.calledWith(spy, 'SUM', 100, 20);
     spy.restore();
   });
 });
