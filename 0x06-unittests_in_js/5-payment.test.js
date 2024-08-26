10dd7e754d682a4ebdc5e4a624a3a1960f086b7f
ouafcode <ouafaesaim@gmail.com>
2024-08-26 22:00:30 +0000

    task5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -10,15 +10,15 @@ describe('sendPaymentRequestToApi', () => {
 
   afterEach(function() {
     spy.restore();
-  )};
+  });
 
   it('check for the console logging is 120', () => {
     sendPaymentRequestToAPI(100, 20);
     sinon.assert.calledOnceWithExactly(spy, 'The total is: 120');
-  )};
+  });
 
   it('check for the console logging is 20', () => {
     sendPaymentRequestToAPI(10, 10);
     sinon.assert.calledOnceWithExactly(spy, 'The total is: 20');
-  )}; 
-)};
+  }); 
+});
