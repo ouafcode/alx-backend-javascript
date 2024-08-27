928b99027f823508829741f70d27a0253c879dfa
ouafcode <ouafaesaim@gmail.com>
2024-08-27 18:23:52 +0000

    task9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,7 +2,7 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Cart page', function () {
-  it('should return status code 200', function (done) {
+  it('tests the server', function (done) {
     request.get('http://localhost:7865', function (error, response, body) {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
@@ -10,17 +10,24 @@ describe('Cart page', function () {
     });
   });
 
-  it('should return 200 when id is mbr', function (done) {
-    request.get('http://localhost:7865/cart/13', function (error, response, body) {
-      expect(responce.statusCode).to.equal(200);
-      expect(body).to.equal('Payment methods for cart 13');
+  it('should return status code 200 when id is a number', function (done) {
+    request.get('http://localhost:7865/cart/12', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
       done();
     });
   });
 
-  it('should return 404 when is not number', function (done) {
+  it('should return status code 404 when id is not a number', function (done) {
     request.get('http://localhost:7865/cart/hello', function (error, response, body) {
-      expect(responce.statusCode).to.equal(404);
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('tests the server with wrong cart id', function (done) {
+    request.get('http://localhost:7865/cart/wrong', function (error, response, body) {
+      expect(response.statusCode).to.equal(404);
       done();
     });
   });
