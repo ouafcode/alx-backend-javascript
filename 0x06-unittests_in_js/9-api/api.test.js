5395bb068fad11947402013e8af33f1cf72b10db
ouafcode <ouafaesaim@gmail.com>
2024-08-27 17:51:05 +0000

    task9

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', function () {
+  it('should return status code 200', function (done) {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should return 200 when id is mbr', function (done) {
+    request.get('http://localhost:7865/cart/13', function (error, response, body) {
+      expect(responce.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 13');
+      done();
+    });
+  });
+
+  it('should return 404 when is not number', function (done) {
+    request.get('http://localhost:7865/cart/hello', function (error, response, body) {
+      expect(responce.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
